72342a7e69776ad69870f6f6e71b96aa94a266cc
Casper van der Wel <caspervdw@gmail.com>
2020-02-23 11:47:13 +0100

    First commit based on shapely

---
+++ config.sh
@@ -0,0 +1,27 @@
+# Config copied from shapely-wheels
+# Test for OSX with [ -n "$IS_OSX" ].
+
+
+function build_geos {
+    build_simple geos $GEOS_VERSION https://download.osgeo.org/geos tar.bz2
+}
+
+
+function pre_build {
+    start_spinner
+    suppress build_geos
+    stop_spinner
+}
+
+
+function run_tests {
+    if [ -n "$IS_OSX" ]; then
+        export PATH=$PATH:${BUILD_PREFIX}/bin
+        export LC_ALL=en_US.UTF-8
+        export LANG=en_US.UTF-8
+    else
+        export LC_ALL=C.UTF-8
+        export LANG=C.UTF-8
+    fi
+    python -m pytest ../pygeos
+}
