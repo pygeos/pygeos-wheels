afbfd6f3ad1f93f8f43b20ee1c95066781dc266c
Casper van der Wel <caspervdw@gmail.com>
2021-01-23 19:45:27 +0100

    Disable ctest on appveyor [skip travis]

--- scripts/build_geos.cmd
+++ scripts/build_geos.cmd
@@ -19,7 +19,9 @@ if exist %GEOS_INSTALL% (
   cd build
   cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=%GEOS_INSTALL% .. || exit /B 1
   cmake --build . --config Release || exit /B 2
-  ctest . --config Release || exit /B 3
+  REM disable ctest because of issue with GEOS 3.9.0
+  REM see https://trac.osgeo.org/geos/ticket/1081
+  REM ctest . --config Release || exit /B 3
   cmake --install . --config Release || exit /B 4
   cd ..
 )
